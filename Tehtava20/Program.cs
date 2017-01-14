7db94f7d348902122b9ed7b4c2b23d1eace32ab5
jerel <jerel@pytis>
2017-01-15 01:24:22 +0200

    mahdollinen tehtävä 20

--- Tehtava20/Program.cs
+++ Tehtava20/Program.cs
@@ -10,9 +10,42 @@ namespace Tehtava20
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("joo");
-            Console.Clear();
-            Console.WriteLine("joo");
+            while (true)
+            {
+                ConsoleKeyInfo info = Console.ReadKey();
+                int val;
+                string[] valilyonti = { " " };
+                int.TryParse(info.KeyChar.ToString(), out val);
+                switch (val)
+                {
+                    case 1:
+                        Console.Beep(260, 500);
+                        break;
+                    case 2:
+                        Console.Beep(290, 500);
+                        break;
+                    case 3:
+                        Console.Beep(320, 500);
+                        break;
+                    case 4:
+                        Console.Beep(340, 500);
+                        break;
+                    case 5:
+                        Console.Beep(380, 500);
+                        break;
+                    case 6:
+                        Console.Beep(420, 500);
+                        break;
+                    case 7:
+                        Console.Beep(470, 500);
+                        break;
+                    case 8:
+                        Console.Beep(500, 500);
+                        break;
+                }
+            }
+
+            }
         }
     }
-}
+
