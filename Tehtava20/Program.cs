9d062e0ae381cda8992dc4cc4bcbf599fd932e51
jerel <jerel@pytis>
2017-01-16 00:48:22 +0200

    hämmentävä vika tehtävässä 20 jota en osaa korjata

--- Tehtava20/Program.cs
+++ Tehtava20/Program.cs
@@ -8,30 +8,45 @@ namespace Tehtava20
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int TaulukkoKoko = 2;
+            int SavelPaikka = 0;
+            int Kosketin;
+            int[] Savelma = new int[TaulukkoKoko];
+            int C = 0, D = 1, E = 2, F = 3, G = 4, A = 5, H = 6, C2 = 7, h = 1;
             while (true)
             {
+                
                 ConsoleKeyInfo info = Console.ReadKey();
-                int val;
-                string[] valilyonti = { " " };
-                int.TryParse(info.KeyChar.ToString(), out val);
-                switch (val)
+                int.TryParse(info.KeyChar.ToString(), out Kosketin);
+                switch (Kosketin)
                 {
                     case 1:
                         Console.Beep(260, 500);
+                        Savelma = new int[TaulukkoKoko + h];
+                        Savelma[SavelPaikka] = C;
+                        h++;
                         break;
                     case 2:
                         Console.Beep(290, 500);
+                        Savelma = new int[TaulukkoKoko + h];
+                        Savelma[SavelPaikka] = D;
+                        h++;
                         break;
                     case 3:
                         Console.Beep(320, 500);
+                        Savelma = new int[TaulukkoKoko + h];
+                        Savelma[SavelPaikka] = E;
+                        h++;
                         break;
                     case 4:
                         Console.Beep(340, 500);
+                        Savelma[SavelPaikka] = F;
                         break;
                     case 5:
                         Console.Beep(380, 500);
+                        Savelma[SavelPaikka] = G;
                         break;
                     case 6:
                         Console.Beep(420, 500);
@@ -42,7 +57,53 @@ namespace Tehtava20
                     case 8:
                         Console.Beep(500, 500);
                         break;
+                    case 0:
+                        for (int i = 1; i < (Savelma.Length - 1); i++)
+                        {
+                            if (Savelma[i] == C)
+                            {
+                                Console.Beep(260, 500);
+                            }
+                            if (Savelma[i] == 1)
+                            {
+                                Console.Beep(290, 500);
+                            }
+                            if (Savelma[i] == 2)
+                            {
+                                Console.Beep(320, 500);
+                            }
+                            if (Savelma[i] == 3)
+                            {
+                                Console.Beep(340, 500);
+                            }
+                            if (Savelma[i] == G)
+                            {
+                                Console.Beep(380, 500);
+                            }
+                            if (Savelma[i] == A)
+                            {
+                                Console.Beep(420, 500);
+                            }
+                            if (Savelma[i] == H)
+                            {
+                                Console.Beep(470, 500);
+                            }
+                            if (Savelma[i] == C2)
+                            {
+                                Console.Beep(500, 500);
+                            }
+
+
+                        }
+                     
+                        break;
                 }
+                if (Kosketin == 9)
+                {
+                    TaulukkoKoko = 1;
+                    SavelPaikka = 0;
+                }
+                SavelPaikka = SavelPaikka + 1;
             }
 
             }
